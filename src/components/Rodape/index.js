f592be4cfdf086f2aa2bc912a9f51011790aa412
renawmontanari <regiangelica@gmail.com>
2024-06-18 19:54:42 -0300

    componentes

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Renan W. Montanari - T6 ONE - Alura.</h2>
+        </footer>
+    )
+}
+
+export default Rodape;
