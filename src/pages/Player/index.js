08460827103b44ff48efe1b18f7577a4922b5aaa
renawmontanari <regiangelica@gmail.com>
2024-06-20 20:46:18 -0300

    pagina-base

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,12 +3,17 @@ import styles from './Player.module.css';
 import Titulo from 'components/Titulo';
 import { useParams } from 'react-router-dom';
 import videos from 'json/db.json';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 function Player() {
     const parametros = useParams();
     const video = videos.find((video) => {
         return video.id === Number(parametros.id);
     })
+
+    if (!video) {
+        return <NaoEncontrada />
+    }
 
     return (
         <>
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "pages/Inicio";
 import Favoritos from "pages/Favoritos";
-import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape";
-import Container from "components/Container";
-import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
+import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route path="*" element={<NaoEncontrada />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
