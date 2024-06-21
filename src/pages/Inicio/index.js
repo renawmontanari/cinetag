ade27ba1007c597daa9c639983167e589fe17661
renawmontanari <regiangelica@gmail.com>
2024-06-21 19:15:10 -0300

    fetch

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,9 +2,19 @@ import styles from './Inicio.module.css';
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json';
+import { useEffect, useState } from 'react';
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/renawmontanari/cinetag-api/videos')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, []);
+
     return (
         <>
             <Banner imagem="home" />
